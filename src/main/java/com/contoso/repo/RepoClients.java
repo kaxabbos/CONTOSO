aaf6c2a55d26b361443c88dd3c5cd7e5cbf041a6
kaxabbos <abbos.kaxramonov@yandex.ru>
2022-05-26 19:53:31 +0300

    2022.05.26.19.53

--- src/main/java/com/contoso/repo/RepoClients.java
+++ src/main/java/com/contoso/repo/RepoClients.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-
 public interface RepoClients extends JpaRepository<Clients, Long> {
     public List<Clients> findAllByOrderByIdDesc();
 }
--- src/main/java/com/contoso/repo/RepoUsers.java
+++ src/main/java/com/contoso/repo/RepoUsers.java
@@ -1,15 +1,12 @@
 package com.contoso.repo;
 
 import com.contoso.models.Users;
-import com.contoso.models.enums.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface RepoUsers extends JpaRepository<Users, Long> {
     Users findByUsername(String username);
-
-    Users findByUsernameAndPassword(String username, String password);
 
     public List<Users> findAllByOrderByRole();
 }
